e03f11c5abd2a33f9ca67028083cb738539c6337
Even Allmighty <smash4321+github@gmail.com>
2015-10-01 10:20:11 +0100

    Improved Setup (allows removal), Fix for Daemon location.

--- findMeDaemon.sh
+++ findMeDaemon.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 #set -x
-cd /var/mobile/Library/findMe
 echo "Startup @ $(date +%d-%m-%Y\ %H:%M:%S)"
 SETTINGSFILE="/var/mobile/Library/Preferences/com.yourcompany.findme.plist"
 interval=$(plutil -key daemon-interval $SETTINGSFILE)
@@ -23,7 +22,7 @@ done
 set -x
 while true
 do
-  ./findMe.sh &
+  /usr/bin/findMe.sh &
   PID=$!
   echo "$(date +%d-%m-%Y\ %H:%M:%S): Start waiting for $interval Minutes."
   echo "Next start is at $(date -d "$interval Minutes")."
--- setup.sh
+++ setup.sh
@@ -4,20 +4,39 @@ if [[ $(whoami) != "root" ]]; then
   exit 1
 fi
 
-cp -r Library/PreferenceLoader/Preferences/findMe/Library/PreferenceLoader/Preferences/findMe
+if [[ "$1" -eq "remove" ]]; then
+   launchctl stop com.yourcompany.findme
+   launchctl unload /Library/LaunchDaemons/com.yourcompany.findme.plist
+   killall -15 Preferences
+   rm -r /Library/PreferenceLoader/Preferences/findMe
+   rm /Library/LaunchDaemons/com.yourcompany.findme.plist
+   killall -15 findMe.sh
+   killall -15 findMeDaemon.sh
+   rm /usr/bin/findMe.sh
+   rm /usr/bin/findMeDaemon.sh
+   echo "Daemon removed"
+   exit
+fi
+
+#Preference Loader
+cp -r Library/PreferenceLoader/Preferences/findMe /Library/PreferenceLoader/Preferences/findMe
 chown -R root:wheel /Library/PreferenceLoader/Preferences/findMe
 chmod -R 755 /Library/PreferenceLoader/Preferences/findMe
 
+#LaunchDaemon
 cp Library/LaunchDaemons/com.yourcompany.findme.plist /Library/LaunchDaemons/com.yourcompany.findme.plist
 chown -R root:wheel /Library/LaunchDaemons/com.yourcompany.findme.plist
 chmod 644 /Library/LaunchDaemons/com.yourcompany.findme.plist
 
-cp var/mobile/Library/Preferences/com.yourcompany.findme.plist /var/mobile/Library/Preferences/com.yourcompany.findme.plist
-chown mobile:mobile /System/Library/LaunchDaemons/com.yourcompany.findme.plist
-chmod 600 /System/Library/LaunchDaemons/com.yourcompany.findme.plist
+#Preference File
+if [[ ! -e /var/mobile/Library/Preferences/com.yourcompany.findme.plist ]]; then
+ cp var/mobile/Library/Preferences/com.yourcompany.findme.plist /var/mobile/Library/Preferences/com.yourcompany.findme.plist
+ chown mobile:mobile /var/mobile/Library/Preferences/com.yourcompany.findme.plist
+ chmod 600 /var/mobile/Library/Preferences/com.yourcompany.findme.plist
+fi 
 
-cp findMe.sh /usr/bin
-chown root:wheel /usr/bin/findMen.sh
+cp findMe.sh /usr/bin/findMe.sh
+chown root:wheel /usr/bin/findMe.sh
 chmod 755 /usr/bin/findMe.sh
 
 cp findMeDaemon.sh /usr/bin
@@ -25,3 +44,7 @@ chown root:wheel /usr/bin/findMeDaemon.sh
 chmod 755 /usr/bin/findMeDaemon.sh
 
 mkdir /var/log/findMe/
+
+launchctl load /Library/LaunchDaemons/com.yourcompany.findme.plist
+launchctl start com.yourcompany.findme
+killall -15 Preferences
